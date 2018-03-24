eda687786e5a7405dfba7ba8d464a29e93667b15
limei <limei@evolve.se>
2018-03-24 10:41:02 +0000

    account

--- lib/atm.rb
+++ lib/atm.rb
@@ -3,7 +3,23 @@ class Atm
   def initialize
     @funds = 1000
   end
-  def withdraw(amount)
-    @funds -= amount
+  def withdraw(amount, account)
+    
+    case
+    when amount > account.balance
+      # we exit the method if the amount we want to withdraw is
+      # bigger than the balance on the account
+      return
+    else
+      # If it's not, we perform the transaction
+      # We DEDUCT the amount from the Atm's funds
+      @funds -= amount
+      # We also DEDUCT the amount from the accounts balance
+      account.balance = account.balance - amount
+      # and we return a responce for a successfull withdraw.
+      { status: true, message: 'success', date: Date.today, amount: amount }
+    end
+
   end
+
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,11 +1,27 @@
 require './lib/atm.rb'
+require 'date'
 
 describe Atm do
+  let(:account) { instance_double('Account') }
+  before do
+    # Before each test we need to add an attribute of `balance`
+    # to the `account` object and set the value to `100`
+    allow(account).to receive(:balance).and_return(100)
+    # We also need to allow `account` to receive the new balance
+    # using the setter method `balance=`
+    allow(account).to receive(:balance=)
+  end
+
   it 'has 1000$ on intitialize' do
     expect(subject.funds).to eq 1000
   end
   it 'funds are reduced at withdraw' do
-    subject.withdraw 50
+    subject.withdraw(50, account)
     expect(subject.funds).to eq 950
   end
+  it 'allow withdraw if account has enough balance.' do
+    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+    expect(subject.withdraw(45, account)).to eq expected_output
+  end
+
 end
