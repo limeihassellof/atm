841eb1c2695f865a579c13749d75cdb2b3fc3a55
limei <limei@evolve.se>
2018-03-25 11:26:28 +0100

    refactoring

--- lib/atm.rb
+++ lib/atm.rb
@@ -4,22 +4,31 @@ class Atm
     @funds = 1000
   end
   def withdraw(amount, account)
-    
+
     case
-    when amount > account.balance
+    when insufficient_funds_in_account?(amount, account)
       # we exit the method if the amount we want to withdraw is
       # bigger than the balance on the account
       return
     else
       # If it's not, we perform the transaction
-      # We DEDUCT the amount from the Atm's funds
-      @funds -= amount
-      # We also DEDUCT the amount from the accounts balance
-      account.balance = account.balance - amount
-      # and we return a responce for a successfull withdraw.
-      { status: true, message: 'success', date: Date.today, amount: amount }
+      perform_transaction(amount, account)
     end
-
   end
+  private
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    # We DEDUCT the amount from the Atm's funds
+    @funds -= amount
+    # We also DEDUCT the amount from the accounts balance
+    account.balance = account.balance - amount
+    # and we return a responce for a successfull withdraw.
+    { status: true, message: 'success', date: Date.today, amount: amount }
+  end
+
 
 end
