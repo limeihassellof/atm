f8eaddfdc85ace14f9bdb58996d267ad16d2196c
limei <limei@evolve.se>
2018-03-27 10:31:56 +0100

    accout

---
+++ lib/account.rb
@@ -0,0 +1,15 @@
+class Account
+  attr_accessor :pin_code, :exp_date,:account_status
+  STANDARD_VALIDITY_YRS = 5
+
+  def initialize
+    @pin_code = rand(1000..9999)
+    @exp_date = set_expire_date
+    @account_status = :active
+  end
+
+  def set_expire_date
+    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+  end
+
+end
---
+++ spec/account_spec.rb
@@ -0,0 +1,21 @@
+require './lib/account.rb'
+require 'date'
+
+describe Account do
+  it 'has 4 digit number for pin code' do
+    number = 1234
+    number_length = Math.log10(number).to_i + 1
+    expect(number_length).to eq 4
+  end
+
+  it 'is expected to have an expiry date on initialize' do
+    # Here we set the validity of the card to 5 yrs as default
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
+  end
+
+  it 'is expected to have :active status on initialize' do
+    expect(subject.account_status).to eq :active
+  end
+
+end
